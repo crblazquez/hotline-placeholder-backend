2c92682f3138f3d50e013afd0afa7e2352263fc2
Moritz Makowski <makowskimoritz@gmail.com>
2020-03-23 19:35:35 +0100

    Initial twilio setup

--- flask_backend/routes.py
+++ flask_backend/routes.py
@@ -1,5 +1,7 @@
 
+from flask import request
 from flask_backend import app
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
 
 @app.route("/")
@@ -7,3 +9,12 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/hotline", methods=['GET', 'POST'])
+def initial_endpoint():
+    """Respond to incoming phone calls with a menu of options"""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    resp.say("Willkommen bei Hilfe am Ohr!", voice="woman", language="de")
+
+    return str(resp)
