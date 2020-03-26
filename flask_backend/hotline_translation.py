b3cbbbc67db849836b8e281e203ff8a66063b815
crblazquez <62717643+crblazquez@users.noreply.github.com>
2020-03-26 20:26:26 +0100

    Update hotline_translation.py
    Fertige Übersetzung

--- flask_backend/hotline_translation.py
+++ flask_backend/hotline_translation.py
@@ -3,10 +3,16 @@ hotline_translation = {
     "choose_language": {
         "de": "Wenn sie auf Deutsch verbunden werden möchten, drücken Sie bitte die 1",
         "en-gb": "If you would like to be connected in english, press 2",
+        "es": "Si desea comunicarse en español, por favor, marque 3.",
+        "fr": "Si vous souhaitez vous communiquer en français, veuillez composer le 4.",
+        "it": "Se desidera comunicarsi in italiano, si prega di premere 5.",
     },
     "choose_language_unknown": {
         "de": "Es tut mir Leid, das habe ich leider nicht verstanden",
         "en-gb": "Sorry, I didn't understand that choice.",
+        "es": "Lo lamento, pero no le he entendido.",
+        "fr": "Je suis désolé, mais je ne vous ai pas compris.",
+        "it": "Mi dispiace, ma non ho capito bene.",
     },
 
 
@@ -17,12 +23,27 @@ hotline_translation = {
         "en-gb": "Thank you for calling and being interested in our service. " +
                  "Since our services are still in development, this call will not " +
                  "be answered.",
+        "es": "Muchas gracias por llamar y por su interés. " +
+              "Como nuestro servicio se encuentra en la fase de desarrollo, " +
+              "no se le devolverá la llamada para esa consulta.",
+        "fr": "Merci beaucoup pour votre appel et pour votre intérêt. " +
+              "Comme notre service n'est qu'en phase de développement, " +
+              "vous ne serez pas rappelé pour cette consultation.",
+        "it": "Grazie per la sua chiamata e per il suo interesse. " +
+              "Poiché il nostro servizio è solo in fase di sviluppo, " +
+              "non sarà richiamato per tale richiesta.",
     },
     "text_2": {
         "de": "Wenn sie unseren Dienst unterstützen möchten, können sie uns jetzt schon " +
               "drei kurze Fragen beantworten. Falls Sie dies nicht tun möchten, legen Sie einfach auf.",
         "en-gb": "If you want to support us on our journey, you can answer three short questions for us. " +
                  "If you don't want that, you can just hang up.",
+        "es": "Si usted quiere colaborar con nosotros, puede contestar ahora a tres breves preguntas. " +
+              "De lo contrario, puede colgar directamente.",
+        "fr": "Si vous souhaitez collaborer avec nous, vous pouvez maintenant répondre " +
+              "à trois courtes questions. Sinon, vous pouvez raccrocher directement.",
+        "it": "Se vuoi collaborare con noi, puoi ora rispondere a tre brevi domande. " +
+              "Altrimenti, puoi riagganciare direttamente.",
     },
 
 
@@ -32,18 +53,33 @@ hotline_translation = {
               "für nein.",
         "en-gb": "Would you make use of our services, either as a caller or as a volunteer." +
                  "Press 1 for yes. Press 2 for no.",
+        "es": "¿Usaría nuestros servicios como alguien que llama o como un voluntario? " +
+              "Marque 1 en caso afirmativo. Marque 2 en caso negativo.",
+        "fr": "Utiliseriez-vous nos services en tant qu'appelant ou en tant que bénévole ? " +
+              "Composez le 1 si oui. Composez le 2 si non.",
+        "it": "Utilizzerebbe i nostri servizi come chiamante o come volontario? " +
+              "Premere 1 se sì. Premere 2 se no.",
     },
     "question_1_answer_yes": {
         "de":  "Vielen Dank für Ihr Interesse!",
         "en-gb": "Thank you for your interest!",
+        "es": "¡Muchas gracias por su interés!",
+        "fr": "Merci beaucopu pour votre intérêt !",
+        "it": "Grazie mille per il suo interesse!",
     },
     "question_1_answer_no": {
         "de": 'Vielen Dank für ihren Anruf! Auf Wiederhören!',
         "en-gb": 'Thank you for your call! Goodbye!',
+        "es": '¡Muchas gracias por llamar! ¡Hasta la próxima!',
+        "fr": 'Merci beaucoup de votre appel ! Au revoir !',
+        "it": 'Grazie mille per la chiamata! Arrivedereci!',
     },
     "question_1_answer_unknown": {
         "de": "Es tut mir Leid, das habe ich leider nicht verstanden.",
         "en-gb": "Sorry, I did not understand that choice.",
+        "es": "Lo lamento, pero no le he entendido.",
+        "fr": "Je suis désolé, mais je ne vous ai pas compris.",
+        "it": "Mi dispiace, ma non ho capito bene.",
     },
 
 
@@ -52,27 +88,48 @@ hotline_translation = {
               "Drücken Sie 1 für Anrufer. Drücken sie 2 für Helfer.",
         "en-gb": "If so, as a caller or as a volunteer. " +
                  "Press 1 for caller. Press 2 for volunteer.",
+        "es": "En ese caso, ¿como alguien que llama o como voluntario? " +
+              "Marque 1 si como alguien que llama. Marque 2 si como voluntario.",
+        "fr": "Le feriez-vous en tant qu'appelant ou en tant que bénévole ? " +
+              "Composez le 1 si en tant qu'appelant. Composez le 2 si comme bénévole.",
+        "it": "Lo faresti come chiamante o come volontario? " +
+              "Premere 1 se come chiamante. Premere 2 se come volontario.",
     },
     "question_2_answer_unknown": {
         "de": "Es tut mir Leid, das habe ich leider nicht verstanden.",
         "en-gb": "Sorry, I did not understand that choice.",
+        "es": "Lo lamento, pero no le he entendido.",
+        "fr": "Je suis désolé, mais je ne vous ai pas compris.",
+        "it": "Mi dispiace, ma non ho capito bene.",
     },
 
 
     "question_3_text_1": {
         "de": "Sie können uns noch mehr unterstützen, indem Sie ihre Postleitzahl angeben.",
         "en-gb": "You can support us even more, if you enter your zip code.",
+        "es": "Puede apoyarnos aún más indicando su código postal.",
+        "fr": "Vous pouvez nous soutenir encore plus en indiquant votre code postal.",
+        "it": "Ci puoi supportare ancora di più indicando il suo codice postale.",
     },
     "question_3_text_2": {
         "de": "Bestätigen Sie mit der Raute-Taste. Um von vorne zu beginnen, warten Sie einfach ab.",
         "en-gb": "Confirm, by using the hash-key. To start over, just hold the line",
+        "es": "Para confirmar, marque almohadilla. Para empezar de nuevo, simplemente espere.",
+        "fr": "Confirmez avec le croisilon. Pour recommencer, attendez simplement.",
+        "it": "Per confermare, segnare il cancelletto. Per ricominciare da capo, basta aspettare.",
     },
     "question_3_answer_yes": {
         "de":  "Vielen Dank für Ihre Teilnahme! Auf Wiederhören!",
         "en-gb": "Thank your for your contribution! Goodbye!",
+        "es": "¡Muchas gracias por su contribución! ¡Hasta la próxima!",
+        "fr": "Merci beaucoup pour votre contribution ! Au revoir !",
+        "it": "Grazie mille per il vostro contributo! Arrivederci!",
     },
     "question_3_answer_unknown": {
         "de": "Bitte geben Sie ihre Postleitzahl an.",
         "en-gb": "Please enter your zip code.",
+        "es": "Por favor, indique su código postal.",
+        "fr": "Veuillez indiquer votre code postal.",
+        "it": "Per favore, indichi il suo codice postale.",
     },
 }
